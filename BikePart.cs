ddb3274d487e62fbd4e1e2d999a7c3d733c1e4e1
ascavin <20521@ecam.be>
2021-02-14 18:15:38 +0100

    Beginning project

---
+++ BikePart.cs
@@ -0,0 +1,13 @@
+namespace Bovelo 
+{
+    class BikePart
+    {
+        public int TimeToBuild = 0;
+        public string Provider = " ";
+        public int Price = 0;
+
+    }
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Bovelo
+{
+    class Program
+    {
+       static void Main(string[] args)
+        {
+            Bike first_bike=new Bike("city","RED",26);
+            Console.WriteLine("first_bike :");
+            Console.WriteLine(first_bike.Type);
+            Console.WriteLine(first_bike.Color);
+            Console.WriteLine(first_bike.Size);
+            first_bike.Color="GREEN";
+            first_bike.Type="explorer";
+            first_bike.Size=28;
+            Console.WriteLine("first_bike changed :");
+            Console.WriteLine(first_bike.Type);
+            Console.WriteLine(first_bike.Color);
+            Console.WriteLine(first_bike.Size);
+
+        }
+    }
+}
